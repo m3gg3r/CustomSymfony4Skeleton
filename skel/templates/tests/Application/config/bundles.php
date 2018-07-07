cb324e72380a4ff69cf2a5519de52eaf355c9685
Tobias Lückel <itsmegger@gmail.com>
2018-07-07 12:51:08 +0200

    Changed to require because 'include_once' if, of course, the wrong

--- skel/templates/tests/Application/config/bundles.php
+++ skel/templates/tests/Application/config/bundles.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-$parentBundles = include_once __DIR__ . '/../../../config/bundles.php';
+$parentBundles =  require __DIR__ . '/../../../config/bundles.php';
 
 return array_merge(
     $parentBundles,
